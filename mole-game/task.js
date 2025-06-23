693ba81dc823eb85f423a004c75560b0c39671d6
Olga <osleptsova@gmail.com>
2025-06-23 11:15:13 +0300

    too commit

--- mole-game/task.js
+++ mole-game/task.js
@@ -6,9 +6,14 @@ for(let index = 0; index<a.length; index++){
 a[index].onclick=()=>{
     if (a[index].className.includes('hole_has-mole'))
     {if(dead.textContent ==10){
-    alert('Победа!')}
+    alert('Победа!');
+    dead.textContent=0;
+    lost.textContent=0;}
     else {dead.textContent = Number(dead.textContent)+1}}
     else{ 
-        if (lost.textContent == 5){alert('Вы проиграли!')}
+        if (lost.textContent == 5){alert('Вы проиграли!');
+            lost.textContent=0;
+            dead.textContent=0;
+        }
         else{lost.textContent = Number(lost.textContent)+1}}}
     }
